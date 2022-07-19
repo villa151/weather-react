fb031eeba3b429c5402fae3fe50c26628faea254
SamC <villa151@umn.edu>
2022-07-19 11:49:38 -0500

    adjust css
    adjust css and className to make similar look to vscode

--- src/Weather.js
+++ src/Weather.js
@@ -12,43 +12,51 @@ export default function Weather() {
     date: "Today's date",
     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
   };
+  let button = (
+    <button type="button" className="here">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-geo-alt-fill"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+      </svg>
+    </button>
+  );
+
+  let form = (
+    <form className="form-inline">
+      <input
+        type="search"
+        placeholder="Type a city..."
+        autofocus="on"
+        autocomplete="off"
+        id="search-input"
+        className="form-control mr-sm-1"
+        aria-label="search"
+      />
+      <button type="submit" value="search" className="btn btn-light">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+      </button>
+    </form>
+  );
+
   return (
     <div className="Weather">
       <div className="weather-heading">
-        <button type="button" className="here">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-geo-alt-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-          </svg>
-        </button>
-        <form className="form-inline">
-          <input
-            type="search"
-            placeholder="Type a city..."
-            autofocus="on"
-            autocomplete="off"
-            id="search-input"
-            className="form-control mr-sm-1"
-            aria-label="search"
-          />
-          <button type="submit" value="search" className="btn btn-light">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </button>
-        </form>
+        {button}
+        {form}
 
         <div className="space"></div>
 
