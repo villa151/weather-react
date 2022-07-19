681eb4502c9c3f48c066ca292b1d90d860d97d93
SamC <villa151@umn.edu>
2022-07-19 09:28:16 -0500

    Clean up app.js
    added main component and cleaned up app.js
    Also posted on netlify

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from "./logo.svg";
+import Weather from "./Weather";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
     </div>
   );
 }
