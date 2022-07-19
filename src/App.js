62ae0f14e55f28bb3b8d067f371c5f16e7843af3
SamC <villa151@umn.edu>
2022-07-19 12:04:50 -0500

    open source added
    code information added at the bottom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p className="footer">
+        {" "}
+        <a
+          href="https://github.com/villa151/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://effulgent-gingersnap-c33a99.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Samantha Cabrera
+        </a>{" "}
+        from
+        <a href="https://shecodes.io/" target="_blank" rel="noreferrer">
+          SheCodes
+        </a>
+      </p>
     </div>
   );
 }
